e35514a95efb12c03aeb6e970fcc0d4fc31cfdff
Yotam Salmon <yotam.salmon@gmail.com>
2018-03-05 22:18:42 +0200

    Base html page for interpreter

---
+++ src/interpreter.js
@@ -0,0 +1,15 @@
+var RIG = function() {
+	
+	
+	/*
+	Execute a code section.
+	Parameters:
+		code - the code string to execute.
+		args - the list of arguments. They will be initially pushed to the stack.
+	*/
+	this.exec = function(code, args) {
+		return 10;
+	}
+};
+
+var rig = new RIG();
