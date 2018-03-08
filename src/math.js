d2c781ef367fba7dace73fd8c2a546f870b9b5f8
Yotam Salmon <yotam.salmon@gmail.com>
2018-03-08 16:00:55 +0200

    Add prime function P

---
+++ src/math.js
@@ -0,0 +1,15 @@
+var is_prime = function(x) {
+    for(var i = 2; i < x; i++)
+        if(x % i === 0) 
+            return false;
+    return x !== 1;
+}; 
+
+/*
+Is a number prime? Element-wise.
+*/
+rig.statements["P"] = new Expression(
+    "P",
+    [],
+    new StringFormatter("stack = stack.map(x => is_prime(x));")
+);
